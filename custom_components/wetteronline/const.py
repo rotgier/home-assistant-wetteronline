2f243abd3353725ee754aa7b5dc4ed0582f3c782
rotgier <rotgier@gmail.com>
2024-08-24 16:09:08 +0200

    Cleaned const

--- custom_components/wetteronline/const.py
+++ custom_components/wetteronline/const.py
@@ -52,27 +52,6 @@ SYMBOLTEXT_CONDITION_MAP: Final[dict[str, str]] = {
     "klar": ATTR_CONDITION_CLEAR_NIGHT,
 }
 
-CONDITION_CLASSES: Final[dict[str, list[int]]] = {
-    ATTR_CONDITION_CLEAR_NIGHT: [33, 34, 37],
-    ATTR_CONDITION_CLOUDY: [7, 8, 38],
-    ATTR_CONDITION_EXCEPTIONAL: [24, 30, 31],
-    ATTR_CONDITION_FOG: [11],
-    ATTR_CONDITION_HAIL: [25],
-    ATTR_CONDITION_LIGHTNING: [15],
-    ATTR_CONDITION_LIGHTNING_RAINY: [16, 17, 41, 42],
-    ATTR_CONDITION_PARTLYCLOUDY: [3, 4, 6, 35, 36],
-    ATTR_CONDITION_POURING: [18],
-    ATTR_CONDITION_RAINY: [12, 13, 14, 26, 39, 40],
-    ATTR_CONDITION_SNOWY: [19, 20, 21, 22, 23, 43, 44],
-    ATTR_CONDITION_SNOWY_RAINY: [29],
-    ATTR_CONDITION_SUNNY: [1, 2, 5],
-    ATTR_CONDITION_WINDY: [32],
-}
-CONDITION_MAP = {
-    cond_code: cond_ha
-    for cond_ha, cond_codes in CONDITION_CLASSES.items()
-    for cond_code in cond_codes
-}
 UPDATE_INTERVAL_WETTERONLINE = timedelta(minutes=15)
 
 CONF_URL_WETTERONLINE: Final = "url_wetteronline"

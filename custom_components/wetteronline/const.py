fb66016681bca89901a2b0755c0230588c75608c
rotgier <rotgier@gmail.com>
2024-09-12 18:48:01 +0200

    Additional mappings

--- custom_components/wetteronline/const.py
+++ custom_components/wetteronline/const.py
@@ -37,22 +37,32 @@ DOMAIN: Final = "wetteronline"
 # MANUFACTURER: Final = "WetterOnline"
 # MAX_FORECAST_DAYS: Final = 4
 
+ATTR_CONDITION_PARTLYSUNNY = "partlysunny"
+
 SYMBOLTEXT_CONDITION_MAP: Final[dict[str, str]] = {
     "sonnig": ATTR_CONDITION_SUNNY,
     "so____": ATTR_CONDITION_SUNNY,
     "Gewitter": ATTR_CONDITION_LIGHTNING_RAINY,
-    "wb____": ATTR_CONDITION_PARTLYCLOUDY,
-    "wechselnd bewölkt": ATTR_CONDITION_PARTLYCLOUDY,
-    "wechselndbewölkt": ATTR_CONDITION_PARTLYCLOUDY,
+    "wb____": ATTR_CONDITION_EXCEPTIONAL,
+    "wechselnd bewölkt": ATTR_CONDITION_EXCEPTIONAL,
+    "wechselndbewölkt": ATTR_CONDITION_EXCEPTIONAL,
     "bw____": ATTR_CONDITION_PARTLYCLOUDY,
     "bewölkt": ATTR_CONDITION_PARTLYCLOUDY,
-    "bws1__": ATTR_CONDITION_RAINY,
-    "bws2__": ATTR_CONDITION_RAINY,
+    "bws1__": ATTR_CONDITION_RAINY,  # light showers
+    "bws2__": ATTR_CONDITION_RAINY,  # showers
+    "bws3__": ATTR_CONDITION_RAINY,  # heavy showers
     "Schauer": ATTR_CONDITION_RAINY,
     "mo____": ATTR_CONDITION_CLEAR_NIGHT,
     "klar": ATTR_CONDITION_CLEAR_NIGHT,
+    "stark bewölkt": ATTR_CONDITION_CLOUDY,  # clouds (ciemne chmury)
+    "md____": ATTR_CONDITION_CLOUDY,  # clouds (ciemne chmury) zaczyna się od m jak "moon" - noc
+    "teils Nebel, teils Sonne": ATTR_CONDITION_FOG
+    + "_partly",  # trochę słońca trochę mgły
+    "ns____": ATTR_CONDITION_FOG + "_partly",  # trochę słońca trochę mgły
+    "Nebel": ATTR_CONDITION_FOG,  # mgła
+    "nb____": ATTR_CONDITION_FOG,  # mgła
 }
 
-UPDATE_INTERVAL_WETTERONLINE = timedelta(minutes=15)
+UPDATE_INTERVAL_WETTERONLINE = timedelta(minutes=1)
 
 CONF_URL_WETTERONLINE: Final = "url_wetteronline"
